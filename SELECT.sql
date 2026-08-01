5c2022e34aca91d9f1dad9cd994a1ae865804b40
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-08-02 00:30:09 +0530

    select command in detail

---
+++ SELECT.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS SQL_DEMO;
+USE SQL_DEMO;
+
+CREATE TABLE Student_Details(
+StudentID INT PRIMARY KEY,
+Name VARCHAR(50),
+email VARCHAR(50) UNIQUE
+);
+
+INSERT INTO Student_Details VALUES(100, "ROHITH", "rohith@gmail.com");
+INSERT INTO Student_Details VALUES(101, "ARJUN", "arjun@gmail.com");
+INSERT INTO Student_Details VALUES(102, "AJAY", "ajay@gmail.com");
+INSERT INTO Student_Details VALUES(103, "SHEEL", "sheel@gmail.com");
+
+SELECT * FROM Student_Details;
